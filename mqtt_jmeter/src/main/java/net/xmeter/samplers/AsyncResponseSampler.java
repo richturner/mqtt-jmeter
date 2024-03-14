4ec4cde2fef280f23f7ba31420e4912311b3f9c2
Rich Turner <7072278+richturner@users.noreply.github.com>
2024-03-14 23:40:24 +0000

    Fixed threading issues

--- mqtt_jmeter/src/main/java/net/xmeter/samplers/AsyncResponseSampler.java
+++ mqtt_jmeter/src/main/java/net/xmeter/samplers/AsyncResponseSampler.java
@@ -5,31 +5,26 @@ import org.apache.jmeter.samplers.SampleResult;
 import org.apache.jmeter.threads.JMeterContextService;
 import org.apache.jmeter.threads.JMeterVariables;
 
-import java.util.logging.Logger;
-
 public class AsyncResponseSampler extends AbstractMQTTSampler {
 	private static final long serialVersionUID = 4360869021667126983L;
 
 	@Override
 	public SampleResult sample(Entry entry) {
 		SampleResult result = new SampleResult();
-		result.setSampleLabel(getName());
-		
+		result.sampleStart();
 		JMeterVariables vars = JMeterContextService.getContext().getVariables();
+		result.setSampleLabel(getName());
 		SubSampler subSampler = (SubSampler) vars.getObject("sub");
 
 		if (subSampler == null) {
-			result.sampleStart();
 			result.setSuccessful(false);
-			result.setResponseMessage("Sub sampler not found.");
+			result.setResponseMessage("Sampler must be preceded by an async sub sampler.");
 			result.setResponseData("Failed. Sub sampler not found.".getBytes());
 			result.setResponseCode("500");
 			result.sampleEnd(); // avoid endtime=0 exposed in trace log
 			return result;
 		}
 
-		// Could make clear responses configurable but cannot think of a good reason to not clear them which allows
-		// the sub sampler to be reused
-		return subSampler.produceAsyncResult(result, true);
+		return subSampler.produceAsyncResult(result);
 	}
 }
--- mqtt_jmeter/src/main/java/net/xmeter/samplers/SubSampler.java
+++ mqtt_jmeter/src/main/java/net/xmeter/samplers/SubSampler.java
@@ -9,40 +9,24 @@ import org.apache.jmeter.threads.JMeterContextService;
 import org.apache.jmeter.threads.JMeterVariables;
 
 import java.text.MessageFormat;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import net.xmeter.SubBean;
-import net.xmeter.samplers.mqtt.MQTTConnection;
-import net.xmeter.samplers.mqtt.MQTTQoS;
-import org.apache.jmeter.samplers.Entry;
-import org.apache.jmeter.samplers.SampleResult;
-import org.apache.jmeter.threads.JMeterContextService;
-import org.apache.jmeter.threads.JMeterVariables;
 
 @SuppressWarnings("deprecation")
 public class SubSampler extends AbstractMQTTSampler {
 	private static final long serialVersionUID = 2979978053740194951L;
 	private static final Logger logger = Logger.getLogger(SubSampler.class.getCanonicalName());
-	
-	private transient MQTTConnection connection = null;
-	private transient String clientId;
-	private boolean subFailed = false;
 
 	private int sampleElapsedTime = 1000;
 	private int sampleCount = 1;
 	private int sampleCountTimeout = 5000;
-
-	private final transient ConcurrentLinkedQueue<SubBean> batches = new ConcurrentLinkedQueue<>();
 	private boolean printFlag = false;
 	private boolean asyncActive = false;
-
-	private final transient Object dataLock = new Object();
+	private final transient Object responseLock = new Object();
 	private boolean lockReleased = false;
 
 	public String getQOS() {
@@ -118,10 +102,12 @@ public class SubSampler extends AbstractMQTTSampler {
 	@Override
 	public SampleResult sample(Entry arg0) {
 		SampleResult result = new SampleResult();
+		result.sampleStart();
 		result.setSampleLabel(getName());
 		JMeterVariables vars = JMeterContextService.getContext().getVariables();
-		connection = (MQTTConnection) vars.getObject(getConnName());
-		clientId = (String) vars.getObject(getConnName()+"_clientId");
+		MQTTConnection connection = (MQTTConnection) vars.getObject(getConnName());
+		String clientId = (String) vars.getObject(getConnName()+"_clientId");
+
 		if (connection == null) {
 			return produceResult(result, 503, "Subscribe failed because connection is not established");
 		}
@@ -146,24 +132,25 @@ public class SubSampler extends AbstractMQTTSampler {
 		}
 		
 		String topicsName = getTopic();
-		setListener(sampleByTime, sampleCount);
-		Set<String> topics = topicsSubscribed.get(clientId);
-		if (topics == null) {
-			logger.severe("subscribed topics haven't been initiated. [clientId: " + (clientId == null ? "null" : clientId) + "]");
-			topics = new HashSet<>();
-			topics.add(topicsName);
-			topicsSubscribed.put(clientId, topics);
-			listenToTopics(topicsName);
-		} else {
-			if (!topics.contains(topicsName)) {
-				topics.add(topicsName);
-				topicsSubscribed.put(clientId, topics);
-				logger.fine("Listen to topic: " + topicsName);
-				result.sampleStart();
-				listenToTopics(topicsName);
+		final ConcurrentLinkedQueue<SubBean> batches = new ConcurrentLinkedQueue<>();
+		vars.putObject("responses", batches);
+
+		connection.setSubListener(((topic, message, ack) -> {
+			ack.run();
+			logger.info("Received message on topic: " + topic);
+			SubBean bean = handleSubBean(batches, sampleByTime, message, sampleCount);
+
+			if(!sampleByTime && bean.getReceivedCount() == sampleCount) {
+				synchronized (responseLock) {
+					lockReleased = true;
+					responseLock.notify();
+				}
 			}
-		}
-		
+		}));
+
+		logger.fine("Subscribe to topic: " + topicsName);
+		boolean subFailed = listenToTopics(connection, topicsName);
+
 		if (subFailed) {
 			// avoid massive repeated "early stage" failures in a short period of time
 			// which probably overloads JMeter CPU and distorts test metrics such as TPS, avg response time
@@ -179,75 +166,85 @@ public class SubSampler extends AbstractMQTTSampler {
 		return doSample(result);
 	}
 
+	@SuppressWarnings("unchecked")
 	private SampleResult doSample(SampleResult result) {
-		JMeterVariables vars = JMeterContextService.getContext().getVariables();
 		boolean sampleByTime = SAMPLE_ON_CONDITION_OPTION1.equals(getSampleCondition());
 
 		if (isAsync() && !asyncActive) {
 			asyncActive = true;
+			JMeterVariables vars = JMeterContextService.getContext().getVariables();
 			vars.putObject("sub", this); // save this sampler as thread local variable for response sampler
+			result.sampleEnd();
 			return produceResult(result, 200, "Subscription created");
 		} else if (sampleByTime) {
 			try {
 				TimeUnit.MILLISECONDS.sleep(sampleElapsedTime);
 			} catch (InterruptedException e) {
 				logger.log(Level.INFO, "Received exception when waiting for notification signal", e);
+				result.sampleEnd();
 				return produceResult(result, 408, "Interrupted");
 			}
-			synchronized (dataLock) {
-				result.sampleStart();
-				return produceResult(result, 200, null);
-			}
+			result.sampleEnd();
+			return produceResult(result, 200, null);
 		} else {
-			synchronized (dataLock) {
-				result.sampleStart();
-				int receivedCount = (batches.isEmpty() ? 0 : batches.element().getReceivedCount());
-				if (receivedCount < sampleCount) {
-					try {
-						if (sampleCountTimeout > 0) {
-							// handle spurious wakeups (https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/lang/Object.html#wait(long,int))
-							long start = System.currentTimeMillis();
-							long sleepMillis = sampleCountTimeout;
+			JMeterVariables vars = JMeterContextService.getContext().getVariables();
+			final ConcurrentLinkedQueue<SubBean> batches = (ConcurrentLinkedQueue<SubBean>) vars.getObject("responses");
+			int receivedCount = (batches.isEmpty() ? 0 : batches.element().getReceivedCount());
 
+			if (receivedCount < sampleCount) {
+				try {
+					if (sampleCountTimeout > 0) {
+						// handle spurious wakeups (https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/lang/Object.html#wait(long,int))
+						long start = System.currentTimeMillis();
+						long sleepMillis = sampleCountTimeout;
+
+						synchronized (responseLock) {
 							while (!lockReleased && sleepMillis > 0) {
-								dataLock.wait(sleepMillis);
+								responseLock.wait(sleepMillis);
 								sleepMillis = sampleCountTimeout - (System.currentTimeMillis() - start);
 							}
-						} else {
+						}
+					} else {
+						synchronized (responseLock) {
 							// handle spurious wakeups (https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/lang/Object.html#wait(long,int))
 							while (!lockReleased) {
-								dataLock.wait();
+								responseLock.wait();
 							}
 						}
-					} catch (InterruptedException e) {
-						logger.log(Level.INFO, "Received exception when waiting for notification signal", e);
-						return produceResult(result, 408, "Interrupted");
 					}
+				} catch (InterruptedException e) {
+					logger.log(Level.INFO, "Received exception when waiting for notification signal", e);
+					result.sampleEnd();
+					return produceResult(result, 408, "Interrupted");
 				}
-				receivedCount = (batches.isEmpty() ? 0 : batches.element().getReceivedCount());
-				if (receivedCount < sampleCount) {
-					result.setSampleCount(receivedCount);
-					return produceResult(result, 408, null);
-				}
-				return produceResult(result, 200, null);
 			}
+			receivedCount = (batches.isEmpty() ? 0 : batches.element().getReceivedCount());
+			result.sampleEnd();
+			if (receivedCount < sampleCount) {
+				return produceResult(result, 408, null);
+			}
+			return produceResult(result, 200, null);
 		}
 	}
 
-	protected SampleResult produceAsyncResult(SampleResult result, boolean clearResponses) {
-		synchronized (dataLock) {
-			SampleResult result1 = doSample(result);
-			if (clearResponses) {
-				batches.clear();
-			}
-			lockReleased = false;
-			return result1;
-		}
+	@SuppressWarnings("unchecked")
+	protected SampleResult produceAsyncResult(SampleResult result) {
+		SampleResult result1 = doSample(result);
+		JMeterVariables vars = JMeterContextService.getContext().getVariables();
+		vars.remove("responses");
+		return result1;
 	}
 
+	@SuppressWarnings("unchecked")
 	private SampleResult produceResult(SampleResult result, int responseCode, String message) {
+		JMeterVariables vars = JMeterContextService.getContext().getVariables();
+		final ConcurrentLinkedQueue<SubBean> batches = (ConcurrentLinkedQueue<SubBean>) vars.getObject("responses");
 		boolean sampleByTime = SAMPLE_ON_CONDITION_OPTION1.equals(getSampleCondition());
 		SubBean bean = batches.poll();
+
+		if (!isAsync()) {
+			vars.remove("responses");
+		}
 
 		if (bean == null) {
 			bean = new SubBean();
@@ -279,12 +276,11 @@ public class SubSampler extends AbstractMQTTSampler {
 		result.setBytes(bean.getReceivedMessageSize());
 		result.setResponseData(contents.toString().getBytes());
 		result.setSampleCount(receivedCount);
-		result.sampleEnd();
 
 		return result;
 	}
 	
-	private void listenToTopics(final String topicsName) {
+	private boolean listenToTopics(final MQTTConnection connection, final String topicsName) {
 		int qos;
 		try {
 			qos = Integer.parseInt(getQOS());
@@ -299,54 +295,38 @@ public class SubSampler extends AbstractMQTTSampler {
 			qos = QOS_0;
 		}
 
+		final AtomicBoolean subFailed = new AtomicBoolean(false);
+
 		connection.subscribe(topicNames,
 				MQTTQoS.fromValue(qos),
 				() -> {
 					logger.fine(() -> "successful subscribed topics: " + String.join(", ", topicNames));
-					synchronized (dataLock) {
-						dataLock.notify();
+					synchronized (connection) {
+						connection.notify();
 					}
 				},
 				error -> {
 					logger.log(Level.INFO, "subscribe failed topics: " + topicsName, error);
-					subFailed = true;
+					subFailed.set(true);
 
-					synchronized (dataLock) {
-						dataLock.notify();
+					synchronized (connection) {
+						connection.notify();
 					}
 				});
 
 		// Block until response
-		synchronized (dataLock) {
+		synchronized (connection) {
 			try {
-				dataLock.wait();
+				connection.wait();
 			} catch (InterruptedException e) {
-				subFailed = true;
+				subFailed.set(true);
 			}
 		}
-	}
-	
-	private void setListener(final boolean sampleByTime, final int sampleCount) {
-		connection.setSubListener(((topic, message, ack) -> {
-			ack.run();
 
-			if(sampleByTime) {
-				synchronized (dataLock) {
-					handleSubBean(sampleByTime, message, sampleCount);
-				}
-			} else {
-				synchronized (dataLock) {
-					SubBean bean = handleSubBean(sampleByTime, message, sampleCount);
-					if(bean.getReceivedCount() == sampleCount) {
-						lockReleased = true;
-						dataLock.notify();
-					}
-				}
-			}
-		}));
+		return subFailed.get();
 	}
-	
-	private SubBean handleSubBean(boolean sampleByTime, String msg, int sampleCount) {
+
+	private SubBean handleSubBean(ConcurrentLinkedQueue<SubBean> batches, boolean sampleByTime, String msg, int sampleCount) {
 		SubBean bean;
 		if(batches.isEmpty()) {
 			bean = new SubBean();
